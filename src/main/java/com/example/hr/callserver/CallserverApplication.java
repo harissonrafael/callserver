35164f5c1f9aa1076c898de6f3f14c77f7a52375
Harisson Rafael <harissonrafael@DESKTOP-KFG0GF0>
2019-09-05 21:09:52 +0100

    Initial version using H2 database

---
+++ src/main/java/com/example/hr/callserver/CallserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr.callserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hr/callserver/CallserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.hr.callserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CallserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
